e6d9973e94a5ba390297e29f1837b590db2d7684
张宇峰的Github <932986370@qq.com>
2020-04-21 14:36:53 +0800

    update1.0.4.21

--- application/api/controller/Index.php
+++ application/api/controller/Index.php
@@ -17,6 +17,6 @@ class Index extends Common
 		];
 		$wechat = Factory::miniProgram($config);//实例化
 		$session_data = $wechat->auth->session($params['mini_code']);
-		$this->json($session_data);
+		return json($session_data);
 	}
 }
